1894b63fd834e4d2594632f16e2ac91b4e79938e
Luke Allcock <121309748+lukeallcodes@users.noreply.github.com>
2023-04-20 17:49:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const memeForm = document.getElementById("meme-form");
 const textForm = document.getElementById("text-form");
 const topTextInput = document.getElementById("top-text-input");
 const bottomTextInput = document.getElementById("bottom-text-input");
+const generateMemeBtn = document.getElementById("generate-meme-btn");
 const memeImage = document.getElementById("meme-image");
 const topText = document.getElementById("top-text");
 const bottomText = document.getElementById("bottom-text");
@@ -17,6 +18,23 @@ textForm.addEventListener("submit", function(event) {
   event.preventDefault();
   topText.textContent = topTextInput.value;
   bottomText.textContent = bottomTextInput.value;
+});
+
+generateMemeBtn.addEventListener("click", function() {
+  const memeCanvas = document.createElement("canvas");
+  memeCanvas.width = memeImage.width;
+  memeCanvas.height = memeImage.height + topText.offsetHeight + bottomText.offsetHeight;
+  const memeContext = memeCanvas.getContext("2d");
+  memeContext.drawImage(memeImage, 0, 0);
+  memeContext.fillText(topText.textContent, 10, topText.offsetTop + topText.offsetHeight);
+  memeContext.fillText(bottomText.textContent, 10, memeImage.height - bottomText.offsetHeight);
+  const memeDataUrl = memeCanvas.toDataURL("image/png");
+  const memeDownloadLink = document.createElement("a");
+  memeDownloadLink.href = memeDataUrl;
+  memeDownloadLink.download = "meme.png";
+  document.body.appendChild(memeDownloadLink);
+  memeDownloadLink.click();
+  document.body.removeChild(memeDownloadLink);
 });
 
 memeImage.addEventListener("load", function() {
